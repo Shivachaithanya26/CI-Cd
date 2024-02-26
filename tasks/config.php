89260f28c50347a29cd7dec631d045c307cec1e3
Shivachaithanya26 <shivachaithanya961@gmail.com>
2024-02-26 14:05:29 +0530

    Adding files to server

---
+++ tasks/config.php
@@ -0,0 +1,21 @@
+<?php
+
+    $db_server = "localhost";
+    $db_user = "shiva";
+    $db_pass = "Shiva";
+    $db_name = "shiva_db";
+
+    // try{
+        $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+    // }
+    // catch(mysqli_sql_exception){
+    //     echo "Could not connect! <br>";
+    // }
+
+    // if($conn){
+    //     echo "You are connected! <br>";
+    // }
+    if($conn === false){
+        die("ERROR: Could not connect. " . mysqli_connect_error());
+    }
+?>
